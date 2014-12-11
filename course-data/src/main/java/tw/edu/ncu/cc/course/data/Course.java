b28153c37787947cda1864f62fc78dea7b8063db
jasonChiu <jason1122g@gmail.com>
2014-12-11 19:56:50 +0800

    create stub services

---
+++ course-data/src/main/java/tw/edu/ncu/cc/course/data/Course.java
@@ -0,0 +1,181 @@
+package tw.edu.ncu.cc.course.data;
+
+
+public class Course {
+
+    public static enum FullHalf {
+        full, half
+    }
+
+    public static enum RequireType {
+        required, elective
+    }
+
+    public static enum PasswordCard {
+        no, optional, all
+    }
+
+    private String no;
+    private String classNo;
+    private String name;
+    private String memo;
+    private String language;
+    private String teachers;
+    private String classRooms;
+    private String time;
+    private boolean isMasterDoctor;
+    private boolean isClosed;
+    private boolean isFirstRun;
+    private boolean isPreSelect;
+    private RequireType type;
+    private FullHalf fullHalf;
+    private PasswordCard passwordCard;
+    private int serialNo;
+    private int maxStudents;
+    private int credit;
+
+    public int getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo( int serialNo ) {
+        this.serialNo = serialNo;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo( String no ) {
+        this.no = no;
+    }
+
+    public String getClassNo() {
+        return classNo;
+    }
+
+    public void setClassNo( String classNo ) {
+        this.classNo = classNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo( String memo ) {
+        this.memo = memo;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage( String language ) {
+        this.language = language;
+    }
+
+    public String getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers( String teachers ) {
+        this.teachers = teachers;
+    }
+
+    public String getClassRooms() {
+        return classRooms;
+    }
+
+    public void setClassRooms( String classRooms ) {
+        this.classRooms = classRooms;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime( String time ) {
+        this.time = time;
+    }
+
+    public boolean isMasterDoctor() {
+        return isMasterDoctor;
+    }
+
+    public void setMasterDoctor( boolean isMasterDoctor ) {
+        this.isMasterDoctor = isMasterDoctor;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public void setClosed( boolean isClosed ) {
+        this.isClosed = isClosed;
+    }
+
+    public boolean isFirstRun() {
+        return isFirstRun;
+    }
+
+    public void setFirstRun( boolean isFirstRun ) {
+        this.isFirstRun = isFirstRun;
+    }
+
+    public boolean isPreSelect() {
+        return isPreSelect;
+    }
+
+    public void setPreSelect( boolean isPreSelect ) {
+        this.isPreSelect = isPreSelect;
+    }
+
+    public RequireType getType() {
+        return type;
+    }
+
+    public void setType( RequireType type ) {
+        this.type = type;
+    }
+
+    public FullHalf getFullHalf() {
+        return fullHalf;
+    }
+
+    public void setFullHalf( FullHalf fullHalf ) {
+        this.fullHalf = fullHalf;
+    }
+
+    public PasswordCard getPasswordCard() {
+        return passwordCard;
+    }
+
+    public void setPasswordCard( PasswordCard passwordCard ) {
+        this.passwordCard = passwordCard;
+    }
+
+    public int getMaxStudents() {
+        return maxStudents;
+    }
+
+    public void setMaxStudents( int maxStudents ) {
+        this.maxStudents = maxStudents;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit( int credit ) {
+        this.credit = credit;
+    }
+
+}
---
+++ course-server/src/main/java/tw/edu/ncu/cc/course/server/controller/api/v1/CourseSearchController.java
@@ -0,0 +1,123 @@
+package tw.edu.ncu.cc.course.server.controller.api.v1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tw.edu.ncu.cc.course.data.Course;
+
+@RestController
+@RequestMapping( value = "api/v1/search" )
+public class CourseSearchController {
+
+
+    @RequestMapping( method = RequestMethod.GET )
+    public ResponseEntity searchCourse( @RequestParam( value = "year", required = false ) Integer year,
+                                        @RequestParam( value = "semester", required = false ) Integer semester,
+                                        @RequestParam( value = "dept", required = false ) String deptartment,
+                                        @RequestParam( value = "week", required = false ) Integer week,
+                                        @RequestParam( value = "period", required = false ) Integer period,
+                                        @RequestParam( value = "limit", required = false ) Integer limit,
+                                        @RequestParam( value = "keyword" ) String keyword,
+                                        @RequestHeader( value = "Accept_Language", required = false ) String language ) {
+        Course course = new Course();
+        course.setSerialNo( 12034 );
+        course.setNo( "EL5001" );
+        course.setClassNo( "*" );
+        course.setName( "文學/文化理論導讀" );
+        course.setClosed( false );
+        course.setMemo( "限三、四年級" );
+        course.setMasterDoctor( false );
+        course.setLanguage( "Chinese" );
+        course.setPasswordCard( Course.PasswordCard.no );
+        course.setFirstRun( true );
+        course.setPreSelect( true );
+        course.setTeachers( "錢夫人,阿土伯" );
+        course.setCredit( 2 );
+        course.setClassRooms( "C2-209,C2-209" );
+        course.setTime( "0-5,2-34" );
+        course.setType( Course.RequireType.required );
+        course.setFullHalf( Course.FullHalf.half );
+        course.setMaxStudents( 0 );
+
+        return new ResponseEntity<>( course, HttpStatus.OK );
+    }
+
+    @RequestMapping( value = "departments/{departmentId}", method = RequestMethod.GET )
+    public ResponseEntity searchCourseOfDepartment( @PathVariable( value = "departmentId" ) String departmentID,
+                                                    @RequestHeader( value = "Accept_Language", required = false ) String language ) {
+        Course course = new Course();
+        course.setSerialNo( 12034 );
+        course.setNo( "EL5001" );
+        course.setClassNo( "*" );
+        course.setName( "文學/文化理論導讀" );
+        course.setClosed( false );
+        course.setMemo( "限三、四年級" );
+        course.setMasterDoctor( false );
+        course.setLanguage( "Chinese" );
+        course.setPasswordCard( Course.PasswordCard.no );
+        course.setFirstRun( true );
+        course.setPreSelect( true );
+        course.setTeachers( "錢夫人,阿土伯" );
+        course.setCredit( 2 );
+        course.setClassRooms( "C2-209,C2-209" );
+        course.setTime( "0-5,2-34" );
+        course.setType( Course.RequireType.required );
+        course.setFullHalf( Course.FullHalf.half );
+        course.setMaxStudents( 0 );
+
+        return new ResponseEntity<>( course, HttpStatus.OK );
+    }
+
+    @RequestMapping( value = "openfor/{openforId}", method = RequestMethod.GET )
+    public ResponseEntity searchCourseForStudent( @PathVariable( value = "openforId" ) String studentID,
+                                                  @RequestHeader( value = "Accept_Language", required = false ) String language ) {
+        Course course = new Course();
+        course.setSerialNo( 12034 );
+        course.setNo( "EL5001" );
+        course.setClassNo( "*" );
+        course.setName( "文學/文化理論導讀" );
+        course.setClosed( false );
+        course.setMemo( "限三、四年級" );
+        course.setMasterDoctor( false );
+        course.setLanguage( "Chinese" );
+        course.setPasswordCard( Course.PasswordCard.no );
+        course.setFirstRun( true );
+        course.setPreSelect( true );
+        course.setTeachers( "錢夫人,阿土伯" );
+        course.setCredit( 2 );
+        course.setClassRooms( "C2-209,C2-209" );
+        course.setTime( "0-5,2-34" );
+        course.setType( Course.RequireType.required );
+        course.setFullHalf( Course.FullHalf.half );
+        course.setMaxStudents( 0 );
+
+        return new ResponseEntity<>( course, HttpStatus.OK );
+    }
+
+    @RequestMapping( value = "summer/{stage}", method = RequestMethod.GET )
+    public ResponseEntity searchCourseInSummerVacation( @PathVariable( value = "stage" ) String stage,
+                                                        @RequestHeader( value = "Accept_Language", required = false ) String language ) {
+        Course course = new Course();
+        course.setSerialNo( 12034 );
+        course.setNo( "EL5001" );
+        course.setClassNo( "*" );
+        course.setName( "文學/文化理論導讀" );
+        course.setClosed( false );
+        course.setMemo( "限三、四年級" );
+        course.setMasterDoctor( false );
+        course.setLanguage( "Chinese" );
+        course.setPasswordCard( Course.PasswordCard.no );
+        course.setFirstRun( true );
+        course.setPreSelect( true );
+        course.setTeachers( "錢夫人,阿土伯" );
+        course.setCredit( 2 );
+        course.setClassRooms( "C2-209,C2-209" );
+        course.setTime( "0-5,2-34" );
+        course.setType( Course.RequireType.required );
+        course.setFullHalf( Course.FullHalf.half );
+        course.setMaxStudents( 0 );
+
+        return new ResponseEntity<>( course, HttpStatus.OK );
+    }
+
+}
